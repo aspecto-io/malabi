abe2ca23e6a2e108c8aee83dc5cca16dc5459c26
Tom Z <tom@aspecto.io>
2021-12-15 18:24:34 +0200

    Update to prerelease version from extjs

--- packages/malabi/src/remote-runner-integration/fetch-remote-telemetry.ts
+++ packages/malabi/src/remote-runner-integration/fetch-remote-telemetry.ts
@@ -1,68 +1,13 @@
 // import { collectorTraceV1Transform } from "opentelemetry-proto-transformations";
 import { initRepository, TelemetryRepository } from 'malabi-telemetry-repository';
-import { SpanAttributes, SpanKind } from '@opentelemetry/api';
-import { ReadableSpan } from '@opentelemetry/sdk-trace-base';
-import { timeInputToHrTime } from '@opentelemetry/core';
-
-// import axios from 'axios';
+// import { SpanAttributes, SpanKind } from '@opentelemetry/api';
+import { convertJaegerSpanToOtel } from 'opentelemetry-span-transformations';
 
 interface FetchRemoteTelemetryProps {
     portOrBaseUrl: string | number;
     currentTestTraceID: string;
 }
 
-const getJaegerValueForTag = (jaegerTagKey, tags) => tags.filter(({ key }) => jaegerTagKey === key)?.[0]?.value;
-const convertJaegerTagsToAttributes = (tags): SpanAttributes => {
-    const spanAttributes: SpanAttributes = {};
-    tags.forEach(({ key, value }) => {
-        spanAttributes[key] = value;
-    });
-    return spanAttributes;
-}
-
-const getOtelKindFromJaegerKind = (jaegerKind: string) => {
-    switch (jaegerKind) {
-        case 'client':
-            return SpanKind.CLIENT;
-        case 'producer':
-            return SpanKind.PRODUCER;
-        case 'server':
-            return SpanKind.SERVER;
-        case 'consumer':
-            return SpanKind.CONSUMER;
-        default:
-            return SpanKind.INTERNAL;
-    }
-}
-
-const convertJaegerSpanToOtel = (jaegerSpan): ReadableSpan => {
-    const durationMillis =  jaegerSpan.duration / 1000;
-    const startDateMillis = jaegerSpan.startTime / 1000;
-    const endDateMillis = timeInputToHrTime(new Date(startDateMillis + durationMillis));
-    return {
-        name: jaegerSpan.operationName,
-        kind: getOtelKindFromJaegerKind(getJaegerValueForTag('span.kind', jaegerSpan.tags)),
-        attributes: convertJaegerTagsToAttributes(jaegerSpan.tags),
-        duration: timeInputToHrTime(durationMillis),
-        startTime: timeInputToHrTime(startDateMillis),
-        endTime: endDateMillis,
-        links: [],
-        spanContext: () => null,
-        instrumentationLibrary: {
-            name: getJaegerValueForTag('otel.library.name', jaegerSpan.tags),
-            version: getJaegerValueForTag('otel.library.version', jaegerSpan.tags),
-        },
-        events: [],
-        ended: true,
-        status: getJaegerValueForTag('otel.status_code', jaegerSpan.tags),
-        resource: jaegerSpan.processID,
-    }
-    // convertedSpan.name = jaegerSpan.operationName;
-    // convertedSpan.kind =
-    // convertedSpan.attributes = convertJaegerTagsToAttributes(jaegerSpan.tags);
-    // convertedSpan.duration = jaegerSpan.duration;
-    // convertedSpan.startTime = jaegerSpan.startTime;
-}
 /**
  * Fetches the spans from the exposed malabi spans endpoint
  * @category Main Functions
