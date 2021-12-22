7b89836a1703459ec738a8b844869eeb41021b29
Tom Z <tom@aspecto.io>
2021-12-22 19:23:37 +0200

    remove log

--- packages/malabi/src/exporter/jaeger.ts
+++ packages/malabi/src/exporter/jaeger.ts
@@ -10,7 +10,6 @@ export const getJaegerSpans = async (serviceName: string) => {
     const JAEGER_PROTOCOL = process.env.MALABI_JAEGER_QUERY_PROTOCOL || 'http';
     const axios = require('axios');
     const res = await axios.get(`${JAEGER_PROTOCOL}://${JAEGER_HOST}:16686/api/traces?service=${serviceName}`)
-    console.log('returning objects', res.data.data.length);
     return res.data.data;
 }
 
